8394d40bc40a06cae46e4f3fe418e01309e3d6d5
FIRST_NAME LAST_NAME <Skiber03@gmail.com>
2023-09-27 12:57:20 +0200

    added test

--- src/Articel.tsx
+++ src/Articel.tsx
@@ -1,13 +1,15 @@
+import './App.css'
+
 export default function Article() {
     return (
         <article>
             <h2>Artikelüberschrift</h2>
             <p>Einleitung des Artikels...</p>
-
-                <p>Listenelement 1</p>
-                <p>Listenelement 2</p>
-                <p>Listenelement 3</p>
-
+                <ul className="test">
+                <li>Listenelement 1</li>
+                <li>Listenelement 2</li>
+                <li>Listenelement 3</li>
+                </ul>
             <a href="http://testwebseite.de/defaultsite">Link zu einer anderen Webseite</a>
             <blockquote>
                 Zitat aus einem anderen Artikel oder Buch.
